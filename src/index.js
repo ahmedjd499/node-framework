e086b1a9c7d6f9e8efea9675db31afe82e7657bf
AhmedUmanlink <ahmed.jaidi@umanlink.com>
2024-10-17 15:17:36 +0100

    some changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require('path');
+const listEndpoints = require('express-list-endpoints');
 
 
 // Import database connection
@@ -11,6 +12,10 @@ const connectDB = require('./configs/database');
 connectDB();
 
 //routes importes
+const clientRoutes = require('./routes/clientRoutes');
+
+const clientViewRoutes = require('./routes/clientViewRoutes');
+
 
 // end routes importes
 
@@ -26,10 +31,24 @@ app.use(express.json());
 
 
 // Use the  routes
+app.use('/api/client', clientRoutes);
+
+app.use('/client', clientViewRoutes);
+
 
 //end use routes
 
 // Start the server
+
+const endpoints = listEndpoints(app);
+console.log(endpoints);
+
+// Example API docs endpoint to display routes
+app.get('/api-docs', (req, res) => {
+  res.json(endpoints);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
